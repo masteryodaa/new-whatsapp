04923f6cde036323d5bd0dee8fcea617045c7e3d
masteryodaa <kujjwal42@gmail.com>
2021-08-06 00:26:48 +0530

    sidebar

--- src/HomeSystem/SidebarChat.js
+++ src/HomeSystem/SidebarChat.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { Avatar } from '@material-ui/core';
+import React, {useState, useEffect} from 'react'
+import {Avatar} from '@material-ui/core';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
-import { db } from '../config'
+import {db} from '../config'
 
 
-function SidebarChat({ addnewChat, name }) {
+function SidebarChat({addnewChat, name}) {
 
-    const [dp, setDP] = useState('');
+    const[dp, setDP]=useState('');
+    
+    useEffect(()=>
+        setDP(Math.floor(Math.random()*1000))
+    ,[])
 
-    useEffect(() =>
-        setDP(Math.floor(Math.random() * 1000))
-        , [])
-
-    const createchat = () => {
+    const createchat = ()=>{
         const chatroom = prompt("Enter your Chatroom name")
 
-        if (chatroom) {
+        if(chatroom){
             db.collection('rooms').add({
-                name: chatroom
+            name: chatroom
             })
         }
     }
@@ -25,22 +25,18 @@ function SidebarChat({ addnewChat, name }) {
     return !addnewChat ? (
         <div class='sidebarchat_item d-flex align-items-center'>
 
-            <div class='d-flex align-items-center'>
-                <Avatar class='avatarImg' alt='' src={`https://avatars.dicebear.com/api/human/${dp}.svg?r=50`} />
+            <Avatar class='avatarImg' alt='' src={`https://avatars.dicebear.com/api/human/${dp}.svg?r=50`}/>
 
-                <div class='chatInfo py-3'>
-                    <h6 class='m-0 p-0'>{name}</h6>
-                    <p class='m-0 p-0'>This is a message.</p>
-                </div>
+            <div class='chatInfo py-3'>
+                <h6 class='m-0 p-0'>{name}</h6>
+                <p class='m-0 p-0'>This is a message.</p>
             </div>
-
-
         </div>
     ) : (
         <div class='newChat' onClick={createchat}>
-            <ChatBubbleIcon />
-            <h3> Add a new chat</h3>
-        </div>
+                <ChatBubbleIcon/>
+               <h3> Add a new chat</h3>
+            </div>
     )
 }
 
